29f34b287b7881f1c20cefab1343591a6c0446fc
Leo <leoshch@gmail.com>
2016-06-08 04:09:56 +0000

    Update Openvpnas_userdata.sh

--- Openvpnas_userdata.sh
+++ Openvpnas_userdata.sh
@@ -9,5 +9,17 @@ sudo apt-get install mailutils -y
 #qwerty
 #qwerty
 #PASSWORD=`openssl rand 8 -base64`
+sudo ovpn-init --ec2
+yes
+yes
+2
+943
+443
+yes
+yes
+yes
+yes
+yes
+
 INSTANCE=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 echo -e "VPN usernam: openvpn,VPN password: ${admin_pw},VPN address:${INSTANCE}" | sudo mail -s "Openvpn Info" antiact@foxmail.com

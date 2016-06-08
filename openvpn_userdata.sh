cde00787e146d9653987d59958138ba8a4683382
Leo <leoshch@gmail.com>
2016-06-08 09:36:15 +0000

    Update openvpn_userdata.sh

--- openvpn_userdata.sh
+++ openvpn_userdata.sh
@@ -4,7 +4,7 @@
 #set openvpn password
 (echo "password"; sleep 1; echo "password")|sudo passwd openvpn
 #Install mail command and send message to maill box
-(echo ;sleep 3;echo ;) | sudo apt-get install mailutils -y
+#(echo ;sleep 3;echo ;) | sudo apt-get install mailutils -y
 INSTANCE=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
-echo -e "VPN usernam: openvpn,VPN password: ${admin_pw},VPN address:${INSTANCE}" | sudo mail -s "Openvpn Info" antiact@foxmail.com
+echo -e "VPN username: openvpn,VPN password: ${admin_pw},VPN address:${INSTANCE}" | sudo mail -s "Openvpn Info" antiact@foxmail.com
 sudo apt-get update -y && sudo apt-get upgrade -y
